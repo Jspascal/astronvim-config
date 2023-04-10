d4d9d1201116d6b8b776ac5b68fcfefb41dd5570
Jspascal <fulgaros@gmail.com>
2023-04-11 00:06:18 +0100

    my actual working configuration

---
+++ lua/plugin.lua
@@ -0,0 +1,41 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+
+		print("Installing packer...")
+
+		vim.cmd([[packadd packer.nvim]])
+
+		return true
+	end
+
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+-- reload neovim whenever plugins.lua is saved
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugin.lua source <afile> | PackerSync
+  augroup end
+]])
+
+return require("packer").startup({
+	function(use)
+		use("wbthomason/packer.nvim")
+
+		if packer_bootstrap then
+			require("packer").sync()
+		end
+	end,
+	config = {
+		display = {
+			open_fn = require("packer.util").float,
+		},
+	},
+})
